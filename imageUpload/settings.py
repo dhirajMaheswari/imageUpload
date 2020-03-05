30da2072f70e0822cd5e14cca316159692b90a21
Dhiraj Maheswari <dhiraje2002@yahoo.com>
2020-03-05 13:58:32 -0500

    Update settings.py

--- imageUpload/settings.py
+++ imageUpload/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*&d=15)60(++%m*y-h2d3dew9p*(+6@$&z6#c8s^hm_8mv_efo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
